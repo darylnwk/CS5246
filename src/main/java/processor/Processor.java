e2b2b61764207dcd762ade3124fd4b43e451500e
Daryl <daryl.n.w.k@gmail.com>
2017-03-23 03:30:31 +0800

    Updated Processor Class

--- src/main/java/processor/Processor.java
+++ src/main/java/processor/Processor.java
@@ -1,6 +1,33 @@
 package processor;
 
 public class Processor {
-    public void naiveBayes(String[] tokens) {}
-    public void negationHandling(String[] tokens) {}
+    /**
+     * This function uses Bernoulli model
+     * @param tokens
+     */
+    public void naiveBayes(String[] tokens) {
+
+    }
+
+    /**
+     * This function negates all tokens following a "not"
+     * @param tokens
+     */
+    public void negationHandling(String[] tokens) {
+        boolean negate = false;
+        for (int i = 0; i < tokens.length; i++) {
+            if (tokens[i].equalsIgnoreCase("not")) {
+                negate = !negate;
+                continue;
+            }
+
+            if (negate) {
+                tokens[i] = negate(tokens[i]);
+            }
+        }
+    }
+
+    private String negate(String token) {
+        return "not_" + token;
+    }
 }
